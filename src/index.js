28e1415c3bda99fb54e9806c9215f36b85d04372
Sanju-UI-Engineer <subbaremi.reddy@zversal.com>
2019-02-07 15:14:50 +0530

    Passing data to data-grid from other component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
